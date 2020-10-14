19f1e8e1f34838492f68f2724ddd073b8faca6c6
Nick Butler <nwmbutler@gmail.com>
2020-10-14 13:35:01 +0100

    pt7 ex 3 completed

--- Activities/Students Version/exercises/pt7_callbacks_promises_and_async_actions/exercise-3.js
+++ Activities/Students Version/exercises/pt7_callbacks_promises_and_async_actions/exercise-3.js
@@ -1,23 +1,37 @@
-const chopAndProcess = (arr) => Promise.resolve(arr)
+function chopAndProcess(arr) {
+  arr.pop();
+  return Promise.resolve(arr);
+}
  
 
 const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
 
+console.log(arr)
 chopAndProcess(arr)
   .then(function(arr) {
-    arr.pop(); console.log(arr); return arr;
+    console.log(arr); 
+    chopAndProcess(arr)
+    return arr;
   })
   .then(function(arr) {
-    arr.pop(); console.log(arr); return arr;
+    console.log(arr); 
+    chopAndProcess(arr);
+    return arr;
   })
   .then(function(arr) {
-    arr.pop(); console.log(arr); return arr;
+    console.log(arr); 
+    chopAndProcess(arr);
+    return arr;
   })
   .then(function(arr) {
-    arr.pop(); console.log(arr); return arr;
+    console.log(arr); 
+    chopAndProcess(arr);
+    return arr;
   })
   .then(function(arr) {
-    arr.pop(); console.log(arr); return arr;
+    console.log(arr); 
+    chopAndProcess(arr);
+    return arr;
   })
   .then(function() {
     console.log("FINISHED!");
